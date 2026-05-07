8e5f405937cd3a3b96d53001d7e7158c99915d6f
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-05-07 16:42:31 -0500

    fix(health): remove health configuration from the project structure

--- src/main/java/com/novaperutech/veyra/platform/health/domain/services/AllergyCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.novaperutech.veyra.platform.health.domain.services;
-
-import com.novaperutech.veyra.platform.health.domain.model.commands.RegisterAllergyCommand;
-
-public interface AllergyCommandService {
-    Long handle(RegisterAllergyCommand command);
-}
--- src/main/java/com/novaperutech/veyra/platform/health/domain/services/VitalSignCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.novaperutech.veyra.platform.health.domain.services;
-
-import com.novaperutech.veyra.platform.health.domain.model.commands.ValidateVitalSignCommand;
-
-public interface VitalSignCommandService {
-    void handle(ValidateVitalSignCommand command);
-}
