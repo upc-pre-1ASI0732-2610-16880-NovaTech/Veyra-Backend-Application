fd703e9017bc9d983aa819ccdf42958cb7154dbe
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-06-20 13:06:12 -0500

    feat(activities): add method to retrieve activity by ID in ActivityQueryService

--- src/main/java/com/novaperutech/veyra/platform/activities/domain/services/ActivityQueryService.java
+++ src/main/java/com/novaperutech/veyra/platform/activities/domain/services/ActivityQueryService.java
@@ -1,10 +1,13 @@
 package com.novaperutech.veyra.platform.activities.domain.services;
 
 import com.novaperutech.veyra.platform.activities.domain.model.queries.GetActivitiesByDateAndNursingHomeQuery;
+import com.novaperutech.veyra.platform.activities.domain.model.queries.GetActivityByIdQuery;
 import com.novaperutech.veyra.platform.activities.domain.model.valueobjects.ActivityView;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ActivityQueryService {
     List<ActivityView> handle(GetActivitiesByDateAndNursingHomeQuery query);
+    Optional<ActivityView> handle(GetActivityByIdQuery query);
 }
