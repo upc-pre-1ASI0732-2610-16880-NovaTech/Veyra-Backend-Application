f7c72dde694ba6bc3f0ebbb9c0341d42463df199
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-07-10 00:00:18 -0500

    feat(contract): add permissions field to ContractResource for enhanced role management

--- src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/resources/ContractResource.java
+++ src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/resources/ContractResource.java
@@ -1,7 +1,8 @@
 package com.novaperutech.veyra.platform.hcm.interfaces.rest.resources;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record ContractResource(Long id,Long staffMemberId,LocalDate startDate, LocalDate endDate, String typeOfContract,
-                               String staffRole, String workShift, String status ) {
+                               String staffRole, String workShift, String status, List<String> permissions) {
 }
