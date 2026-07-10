962b116268f6bebfa2da01085754722d875d1d68
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-07-09 23:40:33 -0500

    feat(medication): enhance MedicationCommandServices to validate nursing home and manage medication administrations

--- src/main/java/com/novaperutech/veyra/platform/nursing/application/internal/queryservices/MedicationQueryServiceImpl.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/application/internal/queryservices/MedicationQueryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.novaperutech.veyra.platform.nursing.application.internal.queryservices;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.Medication;
-import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetAllMedicationsByResidentIdQuery;
+import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.MedicationAdministration;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetAllMedicationsByNursingHomeIdQuery;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetMedicationAdministrationsByResidentIdQuery;
 import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetMedicationByIdQuery;
 import com.novaperutech.veyra.platform.nursing.domain.services.MedicationQueryServices;
+import com.novaperutech.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.MedicationAdministrationRepository;
 import com.novaperutech.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.MedicationRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +16,11 @@ import java.util.Optional;
 @Service
 public class MedicationQueryServiceImpl implements MedicationQueryServices {
     private final MedicationRepository medicationRepository;
+    private final MedicationAdministrationRepository medicationAdministrationRepository;
 
-    public MedicationQueryServiceImpl(MedicationRepository medicationRepository) {
+    public MedicationQueryServiceImpl(MedicationRepository medicationRepository, MedicationAdministrationRepository medicationAdministrationRepository) {
         this.medicationRepository = medicationRepository;
+        this.medicationAdministrationRepository = medicationAdministrationRepository;
     }
 
     @Override
@@ -24,9 +29,13 @@ public class MedicationQueryServiceImpl implements MedicationQueryServices {
     }
 
     @Override
-    public List<Medication> handle(GetAllMedicationsByResidentIdQuery query) {
-        return medicationRepository.findByResidentId(query.id());
+    public List<Medication> handle(GetAllMedicationsByNursingHomeIdQuery query) {
+        return medicationRepository.findByNursingHomeId(query.id());
     }
 
+    @Override
+    public List<MedicationAdministration> handle(GetMedicationAdministrationsByResidentIdQuery query) {
+        return medicationAdministrationRepository.findByResidentIdOrderByCreatedAtDesc(query.residentId());
+    }
 
 }
